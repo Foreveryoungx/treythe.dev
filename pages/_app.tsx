20d66849bf76b3b93b872230ce37408b4d3d190e
Trey Allen <trevon.allen1994@gmail.com>
2023-03-23 21:51:34 -0500

    First Commit Add Chakra UI to page and color scheme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    primary: "#355E3B",
+    secondary: "#D5C7B1",
+    base: "#FFFFFF",
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
